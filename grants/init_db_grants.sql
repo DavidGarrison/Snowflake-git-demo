5d9f842f6f62c1cda97d750d1dffd50cae3b6f3c
David Garrison <dbellgar@gmail.com>
2024-06-12 16:49:18 -0400

    add user to roles

--- grants/init_db_grants.sql
+++ grants/init_db_grants.sql
@@ -2,11 +2,20 @@ create role if not exists git_demo_admin;
 create role if not exists git_demo_analyst;
 create role if not exists git_demo_engineer;
 
+--generally these roles should be granted to a business role or an azure user role
+grant role git_demo_admin to user dgarrison;
+grant role git_demo_analyst to user dgarrison;
+grant role git_demo_analyst to user dgarrison;
+
+--give access to database
 grant usage on database sandbox_db to role git_demo_admin;
 grant usage on database sandbox_db to role git_demo_analyst;
 grant usage on database sandbox_db to role git_demo_engineer;
 
+--allow admin to create a schema
 grant create schema on database sandbox_db to role git_demo_admin;
+
+--give admin access to schema repo and procedures
 grant usage on schema sandbox_db.public to role git_demo_admin;
 grant usage on all procedures in schema sandbox_db.public to role git_demo_admin;
 grant read, write on git repository sandbox_db.public.git_sample to role git_demo_admin;
