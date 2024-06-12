d79a892a9dc6d7a551929d9cf07720e1cac0150b
David Garrison <dbellgar@gmail.com>
2024-06-12 16:57:31 -0400

    add future grant

--- grants/init_schema_grants.sql
+++ grants/init_schema_grants.sql
@@ -1,2 +1,3 @@
 grant usage on schema sandbox_db.git_demo to role git_demo_analyst;
 grant select on all tables in schema sandbox_db.git_demo to role git_demo_analyst;
+grant select on future tables in schema sandbox_db.git_demo to role git_demo_analyst;
