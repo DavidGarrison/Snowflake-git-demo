2cea1ce1160adabdefa2faca106dded5a314cd7a
David Garrison <dbellgar@gmail.com>
2024-06-12 15:43:20 -0400

    fix syntax

--- grants/init_grants.sql
+++ grants/init_grants.sql
@@ -1,3 +1,3 @@
 grant usage on database sandbox_db to role git_demo_analyst;
-grant usage on schema git_demo to role git_demo_analyst;
-grant select on all tables in git_demo to role git_demo_analyst;
+grant usage on schema sandbox_db.git_demo to role git_demo_analyst;
+grant select on all tables in schema sandbox_db.git_demo to role git_demo_analyst;
