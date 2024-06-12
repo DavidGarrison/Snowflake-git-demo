8e2934f68956c5d6ed265ac00fe25d55ec76489e
David Garrison <dbellgar@gmail.com>
2024-06-12 16:52:27 -0400

    fix file name

--- base.sql
+++ base.sql
@@ -1,3 +1,3 @@
 EXECUTE IMMEDIATE FROM './tables/init_tables.sql';
 EXECUTE IMMEDIATE FROM './data/load_demo_table.sql';
-EXECUTE IMMEDIATE FROM './grants/init_grants.sql';
+EXECUTE IMMEDIATE FROM './grants/init_schema_grants.sql';
