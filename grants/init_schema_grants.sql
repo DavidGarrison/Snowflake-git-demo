09f771fe67b658550d4b3d86a86ff0329f89c1e0
David Garrison <dbellgar@gmail.com>
2024-06-27 14:16:59 -0400

    demo changes

--- grants/init_schema_grants.sql
+++ grants/init_schema_grants.sql
@@ -1,3 +1,6 @@
 grant usage on schema sandbox_db.git_demo to role git_demo_analyst;
 grant select on all tables in schema sandbox_db.git_demo to role git_demo_analyst;
-grant select on future tables in schema sandbox_db.git_demo to role git_demo_analyst;
+
+grant role git_demo_analyst to role git_demo_engineer;
+grant insert, update, delete, truncate on all tables in schema sandbox_db.git_demo to rolegit_demo_engineer;
+grant select, insert, update, delete, truncate on future tables in schema sandbox_db.git_demo to rolegit_demo_engineer;
---
+++ view/init_views.sql
@@ -0,0 +1,7 @@
+create or replace view git_demo.demo_view
+as
+select
+    value,
+    count(*) as num_records,
+    max(created_at) as most_recent
+from git_demo.demo_table;
